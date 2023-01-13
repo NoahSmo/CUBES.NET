b6fd41e9abe56f969bb74c213a551d2cd721baa6
umorisseau <ugo.morisseau@enovea.net>
2023-01-13 16:05:10 +0100

    Suppression de l'ancien WebApp

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,49 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {HeaderComponent} from './components/header/header.component';
-import {HomeComponent} from './home/home.component';
-import {ButtonModule} from 'primeng/button';
-import {FooterComponent} from './components/footer/footer.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {CarouselModule} from "primeng/carousel";
-import {MenubarModule} from 'primeng/menubar';
-import {InputTextModule} from "primeng/inputtext";
-import {CarouselHomeComponent} from './carousel-home/carousel-home.component';
-import {CardModule} from "primeng/card";
-import {WineService} from "./services/wine.service";
-import {HttpClientModule} from "@angular/common/http";
-import {ReactiveFormsModule} from "@angular/forms";
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HeaderComponent,
-    HomeComponent,
-    FooterComponent,
-    LoginComponent,
-    RegisterComponent,
-    CarouselHomeComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ButtonModule,
-    CarouselModule,
-    MenubarModule,
-    InputTextModule,
-    CardModule,
-    HttpClientModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
-  providers: [WineService],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
