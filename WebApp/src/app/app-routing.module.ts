baa3a4cfe41386856f585c1339acda43f5fb7a3b
umorisseau <ugo.morisseau@enovea.net>
2023-01-13 16:05:10 +0100

    Suppression de l'ancien WebApp

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {RegisterComponent} from "./register/register.component";
 
-const routes: Routes = [
-  {path: '', component: HomeComponent },
-  {path: 'login', component: LoginComponent },
-  {path: 'register', component: RegisterComponent },
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
