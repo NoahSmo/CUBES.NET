bbff80a826a2f4d028cd8fa5967251951459a423
Noah Petel <noah.petel@gmail.com>
2023-01-29 15:15:24 +0100

    Ajout Domain / Image Controllers

--- Api/Controllers/DomainController.cs
+++ Api/Controllers/DomainController.cs
@@ -14,9 +14,48 @@ namespace Api.Controllers
 
         public DomainController(IDomainService domainService)
         {
-            _domainService = _domainService;
+            _domainService = domainService;
         }
         
+        [HttpGet]
+        public async Task<ActionResult<List<Domain>>> GetDomains()
+        {
+            return await _domainService.GetDomains();
+        }
         
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Domain>> GetDomain(int id)
+        {
+            var result = await _domainService.GetId(id);
+            return result == null ? NotFound("Domain not found") : Ok(result);
+        }
+        
+        [HttpGet("{name}")]
+        public async Task<ActionResult<Domain>> GetDomain(string name)
+        {
+            var result = await _domainService.GetByName(name);
+            return result == null ? NotFound("Domain not found") : Ok(result);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Domain>> CreateDomain(Domain domain)
+        {
+            var result = await _domainService.CreateDomain(domain);
+            return result == null ? Unauthorized("Domain already exist") : Ok(result);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Domain>> UpdateDomain(int id, Domain domain)
+        {
+            var result = await _domainService.UpdateDomain(id, domain);
+            return result == null ? NotFound("Domain not found") : Ok(result);
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Domain>> DeleteDomain(int id)
+        {
+            var result = await _domainService.DeleteDomain(id);
+            return result == null ? NotFound("Domain not found") : Ok(result);
+        }
     }
 }
