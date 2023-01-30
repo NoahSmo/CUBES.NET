241426c951b9888e360b5f123b74272302d68989
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-01-30 16:28:54 +0100

    login fonctionnel

--- WpfApp/Models/User.cs
+++ WpfApp/Models/User.cs
@@ -2,6 +2,16 @@
 
 public class User
 {
+    public class UserLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+
+
+    public class User : INotifyPropertyChanged
+    {
     public int Id { get; set; }
         
     public string Username { get; set; }
