f266c7bec2906b78458f3f0292edc57b396b65c4
theoloyaute <theo.loyaute05@gmail.com>
2023-02-01 11:24:15 +0100

    Adding view login

--- WebApp/src/app/login/login.component.ts
+++ WebApp/src/app/login/login.component.ts
@@ -1,7 +1,4 @@
-import {Component} from '@angular/core';
-import {AuthentificationService} from "../services/authentification.service";
-import {Router} from "@angular/router";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -10,36 +7,7 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class LoginComponent {
 
-  email = '';
-  password = '';
-  wrongCredentials = false;
-  userFormGroup! : FormGroup;
+  constructor() {}
 
-  constructor(
-    private authService: AuthentificationService,
-    private router: Router,
-    private fb: FormBuilder
-  ) {
-  }
-
-  ngOnInit() {
-    this.userFormGroup = this.fb.group({
-      email : this.fb.control(""),
-      password : this.fb.control("")
-    });
-  }
-
-  show: boolean = false;
-
-  login() {
-    this.wrongCredentials = false;
-    this.email = this.userFormGroup.value.email;
-    this.password = this.userFormGroup.value.password;
-    this.authService.login(this.email, this.password).subscribe(result => {
-      this.router.navigate(['/']);
-
-    }, error => {
-      this.wrongCredentials = true;
-    });
-  }
+  ngOnInit(): void{}
 }
