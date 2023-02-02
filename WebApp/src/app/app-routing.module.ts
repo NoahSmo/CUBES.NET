0b3e8b779d36167a9bb03889f902e4949a0925c5
theoloyaute <theo.loyaute05@gmail.com>
2023-02-02 09:10:07 +0100

    Adding view register

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'connexion', component: LoginComponent },
+  {path: 'inscription', component: RegisterComponent },
+
 ];
 
 @NgModule({
