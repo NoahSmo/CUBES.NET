8fd7512945178c09cf0239482869c73af3144a54
umorisseau <ugo.morisseau@enovea.net>
2023-02-03 14:21:14 +0100

    Add: Navbar + Update: header & style.scss

--- WebApp/src/app/components/navbar/navbar.component.ts
+++ WebApp/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  @Input()
+  public items: MenuItem[] = [
+    {
+      label: 'Collection',
+      icon: 'pi pi-wallet',
+      items: [
+        {
+          label: 'Toute notre collection',
+          icon: '',
+        },
+        {
+          separator: true
+        },
+        {
+          label: 'Vin blanc',
+          icon: ''
+        },
+        {
+          label: 'Vin rouge',
+          icon: ''
+        },
+      ]
+    },
+    {
+      label: 'Présentation',
+      icon: 'pi pi-fw pi-pencil',
+    },
+    {
+      label: 'Nous contacter',
+      icon: 'pi pi-fw pi-user',
+    },
+    {
+      separator: true
+    }
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
 
 }
