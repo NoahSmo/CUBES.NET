698a0f157a857cef61b95ee21e7ac4796b6a49d2
umorisseau <ugo.morisseau@enovea.net>
2023-02-21 09:49:24 +0100

    Add: WineService & carousel

--- Api/Program.cs
+++ Api/Program.cs
@@ -113,7 +113,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
--- WebApp/src/app/services/wine.service.ts
+++ WebApp/src/app/services/wine.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WineService {
 
-  constructor() { }
+  protected baseUrl = 'http://localhost:38387/api';
+  protected componentUrl = this.baseUrl + '/Article';
+
+  constructor(
+    protected router: Router,
+    protected http: HttpClient
+  ) {
+  }
+
+  getArticles(): Observable<any>{
+    const params: HttpParams = new HttpParams();
+    return this.http.get(this.componentUrl, {params})
+  }
+
 }
