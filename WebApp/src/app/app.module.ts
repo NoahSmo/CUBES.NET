7eaa610173d9dfbda2e6afc482ec2d54d745d8a1
umorisseau <ugo.morisseau@enovea.net>
2023-02-21 09:49:24 +0100

    Add: WineService & carousel

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {HeaderComponent} from './components/header/header.component';
+import {HomeComponent} from './home/home.component';
 import {ButtonModule} from 'primeng/button';
-import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './login/login.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {LoginComponent} from './login/login.component';
 import {MenuModule} from "primeng/menu";
-import { RegisterComponent } from './register/register.component';
+import {RegisterComponent} from './register/register.component';
 import {CarouselModule} from "primeng/carousel";
 import {MenubarModule} from 'primeng/menubar';
 import {InputTextModule} from "primeng/inputtext";
-import { CarouselHomeComponent } from './carousel-home/carousel-home.component';
+import {CarouselHomeComponent} from './carousel-home/carousel-home.component';
+import {CardModule} from "primeng/card";
+import {WineService} from "./services/wine.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -35,8 +38,11 @@ import { CarouselHomeComponent } from './carousel-home/carousel-home.component';
     CarouselModule,
     MenubarModule,
     InputTextModule,
+    CardModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WineService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
