6f81fda0a63fe7b2d4886f26519fbe6247393f83
Noah <90006474+NoahSmo@users.noreply.github.com>
2023-02-23 22:37:34 +0100

    Modif Models

--- Api/Models/Article.cs
+++ Api/Models/Article.cs
@@ -20,13 +20,17 @@ namespace Api.Models
         public Domain? Domain { get; set; }
         
         
+        public int? ProviderId { get; set; }
+        public Provider? Provider { get; set; }
+        
+        
         public int? CategoryId { get; set; }
         public Category? Category { get; set; }
         
         
+        
         public virtual List<ArticleOrder>? ArticleOrders { get; set; }
         public virtual List<CartItem>? CartItems { get; set; }
-        public virtual List<Provider>? Providers { get; set; }
         public virtual List<Image>? Images { get; set; }
         public virtual List<Comment>? Comments { get; set; }
     }
@@ -35,7 +39,8 @@ namespace Api.Models
     {
         public int Id { get; set; }
         public int ArticleId { get; set; }
-        public int OrderId { get; set; }
+        public int? OrderId { get; set; }
+        public int? ProviderOrderId { get; set; }
         public int Quantity { get; set; }
     }
 }
