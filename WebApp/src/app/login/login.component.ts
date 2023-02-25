997d4fe6c27508dd0fcec53483ad586a6dfe001b
umorisseau <ugo.morisseau@enovea.net>
2023-02-25 13:20:54 +0100

    update: AuthentificationService

--- WebApp/src/app/login/login.component.ts
+++ WebApp/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthentificationService} from "../services/authentification.service";
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-login',
@@ -7,13 +11,38 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  constructor() {}
+  email = '';
+  password = '';
+  wrongCredentials = false;
+  userFormGroup! : FormGroup;
+  currentUser: User | undefined;
 
-  ngOnInit() {}
+  constructor(
+    private authService: AuthentificationService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+  }
+
+  ngOnInit() {
+    this.userFormGroup = this.fb.group({
+      email : this.fb.control(""),
+      password : this.fb.control("")
+    });
+
+  }
 
   show: boolean = false;
 
-  password() {
-    this.show = !this.show;
+  login() {
+    this.wrongCredentials = false;
+    this.email = this.userFormGroup.value.email;
+    this.password = this.userFormGroup.value.password;
+    this.authService.loginDesktop(this.email, this.password).subscribe(result => {
+      this.router.navigate(['/']);
+
+    }, error => {
+      this.wrongCredentials = true;
+    });
   }
 }
